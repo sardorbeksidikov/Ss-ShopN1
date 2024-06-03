6ce7f41537de2425a1f225673fb836e2e52864b1
sardorbeksidikov <sardorsidikov005@gmail.com>
2024-06-03 16:06:22 +0500

    sardorbek

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -74,7 +74,7 @@ const ShoppingCart = () => {
   return (
     <>
       {products?.length ? (
-        <div className="h-screen bg-gray-100 pt-20">
+        <div className="h-screen bg-gray-100 mt-28">
           <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
           <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
             <div className="rounded-lg md:w-2/3">
